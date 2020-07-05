133b725dda874b89b837881f99737267b54fe213
hansputera <47862061+hansputera@users.noreply.github.com>
2020-07-05 15:55:40 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+         "fmt" 
+         "net/http"
+         "os"
+        )
+
+
+func main() {
+
+ port := Getenv("PORT")
+
+ http.HandleFunc("/", func (w.httpResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "This is a simple app!")
+ }
+
+ fs := http.FileServer(http.Dir("public/"))
+ http.Handle("/public/", http.StripPrefix("/public/", fs))
+ 
+ http.ListenAndServe(":%d", port)
+}
